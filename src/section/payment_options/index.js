7ffea67168ba94f8ddde665cebe6e9de624061ae
amr ashour <amrbahy1996@gmail.com>
2022-03-16 17:36:27 +0200

    changed the icon color

--- src/section/payment_options/index.js
+++ src/section/payment_options/index.js
@@ -20,7 +20,7 @@ const PaymentMethod = () => {
             <div className="md:col-span-4">
                 {Data.map(d => (
                     <div onClick={() => handleActive(d.id)}  className={ d.id === active ? "flex px-5 py-5 my-4 rounded-2xl shadow-xl cursor-pointer text-white bg-purple-700 border-2 border-purple-700" : "flex px-5 py-5 my-4 rounded-2xl shadow-xl cursor-pointer hover:text-white hover:bg-purple-700 text-black duration-500 border-2 border-purple-300"}>
-                        <div className={d.id === active ? "w-11 text-xl mt-1" : "w-11 text-2xl  mt-1 hover:text-white"}>{d.icon}</div>
+                        <div className={d.id === active ? "w-11 text-xl mt-1" : "w-11 text-2xl  mt-1 text-purple-700"}>{d.icon}</div>
                         <div className="ml-5 md:text-2xl text-xl font-medium">{d.title}</div>
                     </div>
                 ))}
