9632f947b772218f3496300654ccd0693df83a1b
amr ashour <amrbahy1996@gmail.com>
2022-03-18 08:05:29 +0200

    i fixed the hover effect on tab cards

--- src/section/payment_options/TabPaymentMethod.js
+++ src/section/payment_options/TabPaymentMethod.js
@@ -3,6 +3,9 @@ import React, {useState} from "react";
 //data for both of tabs and input fields.
 import Data from './data'
 
+//style
+import "./style.css";
+
 const TabPaymentMethod = () => {
 
     //the state and function for handling the active tab and it's contents
@@ -18,8 +21,8 @@ const TabPaymentMethod = () => {
         <div className="hidden md:grid md:grid-cols-12 gap-5">
             <div className="md:col-span-4">
                 {Data.map(d => (
-                    <div onClick={() => handleActive(d.id)}  className={ d.id === active ? "flex p-4 my-4 rounded-2xl shadow-xl cursor-pointer text-white bg-purple-700 border-2 border-purple-700" : "flex p-4 my-4 rounded-2xl shadow-xl cursor-pointer hover:text-white hover:bg-purple-700 text-black duration-500 border-2 border-purple-300"}>
-                        <div className={d.id === active ? "w-11 text-xl mt-1" : "w-11 text-xl  mt-1 text-purple-700"}>{d.icon}</div>
+                    <div onClick={() => handleActive(d.id)}  className={ d.id === active ? "card flex p-4 my-4 rounded-2xl shadow-xl cursor-pointer text-white bg-purple-700 border-2 border-purple-700" : "card flex p-4 my-4 rounded-2xl shadow-xl cursor-pointer hover:text-white hover:bg-purple-700 text-black duration-500 border-2 border-purple-300"}>
+                        <div className={d.id === active ? "icon w-11 text-xl mt-1 text-white" : "icon w-11 text-xl text-purple-700  mt-1"}>{d.icon}</div>
                         <div className="ml-5 md:text-sm lg:text-base font-medium">{d.title}</div>
                     </div>
                 ))}
