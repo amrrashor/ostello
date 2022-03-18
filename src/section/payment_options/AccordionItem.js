a509b02e29dab44e4cff8c61bfa459cd94005ae3
amr ashour <amrbahy1996@gmail.com>
2022-03-18 11:42:37 +0200

    some modifications in the accordion items

--- src/section/payment_options/AccordionItem.js
+++ src/section/payment_options/AccordionItem.js
@@ -8,9 +8,9 @@ const AccordionItem = ({ currentvalue }) => {
 
     return (
         <div key={currentvalue.id} className="px-5 py-5cursor-pointer">
-            <div onClick={() => setToggle(!toggle)} className={toggle ? " flex rounded-xl bg-purple-700 p-4 mb-1" : "card-border flex rounded-2xl shadow-xl p-4 mb-2"}>
+            <div onClick={() => setToggle(!toggle)} className={toggle ? " flex radius bg-color-one  p-4 mb-1" : "card-border flex radius p-4 mb-2"}>
                 <div className={ toggle ? "w-11 text-xl mt-1 text-white" : "w-11 text-xl text-purple-700 mt-1"}>{ currentvalue.icon }</div>
-                <h3 className={toggle ? "ml-5 text-white" : "ml-5"}>{currentvalue.title}</h3>
+                <h3 className={toggle ? "ml-5 text-white text-xl font-medium text-white" : "font text-xl font-medium ml-5"}>{currentvalue.title}</h3>
                 <span className={toggle ? "mt-1 text-white ml-auto" : "mt-1 ml-auto"}>{toggle ? <BsChevronUp /> : <BsChevronDown />}</span>
             </div>
             {toggle && <p className="mb-4">{currentvalue.input}</p>}
--- src/section/paymnet_details/coupon_card/index.js
+++ src/section/paymnet_details/coupon_card/index.js
@@ -5,14 +5,14 @@ import { BiChevronRight } from 'react-icons/bi';
 import './style.css'
 const CouponCard = () => {
     return (
-        <div className="coupon w-full mt-6 shadow radius px-7 py-4 text-xl font-bold">
+        <button className="coupon w-full mt-6 shadow radius px-7 py-4 text-xl font-bold">
             <div className="flex justify-between">
                 <div className="text-lg">
                     <RiCoupon3Line className="inline mr-1 mb-1 text-2xl" />Use coupons
                 </div>
                 <BiChevronRight className="inline mr-1 mt-1 text-2xl" />
             </div>
-        </div>
+        </button>
     )
 }
 
