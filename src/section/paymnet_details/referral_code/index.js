c08f780df519aaf8fd66bcb7d9cef23c7993bfa8
amr ashour <amrbahy1996@gmail.com>
2022-03-18 11:09:32 +0200

    finished refferal input

--- src/section/paymnet_details/referral_code/index.js
+++ src/section/paymnet_details/referral_code/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import './style.css'
 const Referral = () => {
     return (
-        <div className="flex mt-6  shadow-xl text-gray-600 px-7 py-4">
-            <input type="text" placeholder="Type referral code here" className="w-11/12 py-1 px-3 rounded-xl border border-dashed border-yellow-500 bg-yellow-50" />
-            <button className="py-1 px-3 text-green-600 border border-green-600 rounded-xl ml-4">Redeem</button>
+        <div className="flex mt-6 radius shadow  px-7 py-4">
+            <input type="text" placeholder="Type referral code here" className="input-refferal w-full py-1 px-3 rounded-lg" />
+            <button className="py-1 px-3 rounded-xl ml-4 font-bold">Redeem</button>
         </div>
 
     )
